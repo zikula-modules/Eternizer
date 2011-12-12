fd4d3db80a25cf61b6c5ded1174d463bee90ac47
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2011-12-12 13:15:46 +0100

    news structure

---
+++ Eternizer/lib/Eternizer/Form/Handler/Admin/configHandler.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2007, Philipp Niethammer
+ * @link http://www.guite.de
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author    Philipp Niethammer <webmaster@nochwer.de>
+ * @package Zikula
+ * @subpackage Eternizer
+ */
+
+
+class Eternizer_Form_Handler_Admin_configHandler extends Zikula_Form_AbstractHandler {
+
+    var $init;
+    
+
+    public function __construct($args)
+    {
+        $this->init = $init;
+    }
+
+  /*  function Eternizer_admin_configHandler($init=false)
+    {
+        $this->init = (bool) $init;
+    }*/
+
+    function initialize(Zikula_Form_View $view) {
+        $dom = ZLanguage::getModuleDomain('Eternizer');
+        if (!SecurityUtil::checkPermission(($this->init?'':'Eternizer').'::', '::', ACCESS_ADMIN)) {
+            return $render->pnFormSetErrorMsg(__('Sorry! No authorization to access this module.', $dom));
+        }
+
+        if (!$this->init) {
+            $modvars = pnModGetVar('Eternizer');
+        } else {
+            $modvars = pnModFunc('Eternizer', 'init', 'getDefaultConfig');
+        }
+
+        $modvars['orderItems']         = array(array( 'value' => 'asc',
+                                                      'text'  => __('ascending', $dom)),
+                                            array(    'value' => 'desc',
+                                                      'text'  => __('descending', $dom)));
+
+        $modvars['spammodeItems']   = array(array(    'value' => '0',
+                                                      'text'  => __('No', $dom)),
+                                            array(    'value' => '1',
+                                                      'text'  => __('Guests only, Moderate', $dom)),
+                                            array(    'value' => '2',
+                                                      'text'  => __('Guests only, Reject', $dom)),
+                                            array(    'value' => '3',
+                                                      'text'  => __('All, Moderate', $dom)),
+                                            array(    'value' => '4',
+                                                      'text'  => __('All, Reject', $dom)));
+
+        $modvars['moderateItems']    = array(array(   'value' => '0',
+                                                      'text'  => __('No', $dom)),
+                                            array(    'value' => '1',
+                                                      'text'  => __('Guests only', $dom)),
+                                            array(    'value' => '2',
+                                                      'text'  => __('All users', $dom)));
+
+        $modvars['wwactionItems']     = array(array(  'value'  => 'nothing',
+                                                      'text'   => __('No', $dom)),
+                                            array(    'value'  => 'truncate',
+                                                      'text'   => __('truncate', $dom)),
+                                            array(    'value'  => 'wrap',
+                                                      'text'   => __('wrap', $dom)));
+
+        $modvars['pnnotify_available'] = (bool) pnModAvailable('pn_notify');
+
+        $selects['mandatoryItems']    = array(array(  'value'   => 0,
+                                                      'text'    => __('Optional', $dom)),
+                                            array(    'value'   => 1,
+                                                      'text'    => __('anonymous users', $dom)),
+                                            array(    'value'   => 2,
+                                                      'text'    => __('All', $dom)));
+        $selects['typeItems']   = array(array(        'value'   => 'name',
+                                                      'text'    => __('Name', $dom)),
+                                            array(    'value'   => 'mail',
+                                                      'text'    => __('E-mail address', $dom)),
+                                            array(    'value'   => 'url',
+                                                      'text'    => __('URL', $dom)),
+                                            array(    'value'   => 'text',
+                                                      'text'    => __('Text', $dom)));
+
+        $selects['titlefieldItems'] = array();
+        foreach ($modvars['profile'] as $k => $v) {
+            $selects['titlefieldItems'][] = array('value' => $k, 'text' => $this->__($v['title'])); // pnML deleted
+        }
+
+        $profileItems = pnModAPIFunc('Profile', 'user', 'getallactive');
+
+        $selects['profileItems'] = array(array('value' => '0', 'text' => __('No', $dom)));
+        foreach ($profileItems as $field) {
+            if ($field['prop_viewby'] == 0 && $field['prop_displaytype'] < 2) {
+                $selects['profileItems'][] = array('value' => $field['prop_label'],
+                                                    'text'  => $this->__($field['prop_label'])); // pnML deleted
+            }
+        }
+
+        $modvars['init'] = $this->init;
+
+        $this->view->assign($modvars);
+        $this->view->assign($selects);
+
+        return true;
+    }
+
+    function handleCommand(Zikula_Form_View $view, &$args)
+      {
+        switch($args['commandName']) {
+        case 'update':
+            if (!$this->view->isValid())
+            return false;
+
+              $data = $this->view->getValues();
+
+              ModUtil::delVar($this->name);
+
+            $profile = array();
+            $newprofile = array();
+              foreach ($data['profile'] as $k => $v) {
+                  $info = explode('_', $k);
+
+                  if (substr($info[1], 0, 1) == 'n') {
+                      $newprofile[$info[1]][$info[2]] = $v;
+                  } else {
+                      $profile[$info[1]][$info[2]] = $v;
+                  }
+              }
+              foreach ($newprofile as $v) {
+                  if (!empty($v['title']))
+                      $profile[] = $v;
+              }
+
+              $posarray = array();
+            foreach (array_keys($profile) as $k) {
+                if (!empty($profile[$k]['title'])) {
+                    $posarray[$profile[$k]['pos']] = $k;
+                }
+            }
+
+            ksort($posarray);
+
+            $sortprofile = array();
+            foreach ($posarray as $k) {
+                $sortprofile[$k] = $profile[$k];
+            }
+            $profile =& $sortprofile;
+
+              unset($data['profile']);
+              foreach ($data as $k => $v) {
+                  pnModSetVar('Eternizer', $k, $v);
+              }
+
+              pnModSetVar('Eternizer', 'profile', $profile);
+
+              if (!$this->init) {
+                  $url = pnModUrl('Eternizer', 'admin', 'config');
+              } else {
+                  $url = pnModUrl('Eternizer', 'init', 'step3');
+              }
+
+              return $this->view->redirect($url);
+              break;
+          case 'cancel':
+          default:
+              if (!$this->init) {
+                  $url = pnModUrl('Eternizer', 'admin', 'config');
+              } else {
+                  $url = pnModUrl('Eternizer', 'init', 'step2');
+              }
+              return $this->view->redirect($url);
+        }
+
+        return true;
+      }
+}
--- Eternizer/templates/Eternizer_user_form.tpl
+++ Eternizer/templates/Eternizer_user_form.tpl
@@ -1,29 +1,29 @@
 {* $Id$ *}
-{pnformvalidationsummary}
-{pnform cssClass="z-form"}
+{formvalidationsummary}
+{form cssClass="z-form"}
 <div>
     <fieldset>
         <legend>{gt text="Add a new entry"}</legend>
         {foreach from=$profile key=id item=item}
         <div class="z-formrow">
-            {pnformlabel for="profile_$id" text=$item.title}
+            {formlabel for="profile_$id" text=$item.title}
             {if $item.type eq 'mail'}
-            {pnformemailinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
+            {formemailinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
             {elseif $item.type eq 'url'}
-            {pnformurlinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
+            {formurlinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
             {else}
-            {pnformtextinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
+            {formtextinput id="profile_$id" group="profile" text=$item.value maxLength='255' mandatory=$item.mandatory readOnly=$item.readonly}
             {/if}
         </div>
         {/foreach}
 
         <div class="z-formrow">
-            {pnformlabel for='text' __text='Text'}
-            {pnformtextinput id='text' textMode="multiline" mandatory='1' rows="5" cols="40"}
+            {formlabel for='text' __text='Text'}
+            {formtextinput id='text' textMode="multiline" mandatory='1' rows="5" cols="40"}
 
             {if $bbcode}
             <div class="z-formnote">
-                {pnmodfunc modname=bbcode type=user func=bbcodes textfieldid=text}
+                {modfunc modname=bbcode type=user func=bbcodes textfieldid=text}
             </div>
             {/if}
             {if $bbsmile}
@@ -37,8 +37,8 @@
     </fieldset>
 
     <div class="z-formbuttons">
-        {pnformbutton commandName="create" __text="Create"}
-        {pnformbutton commandName="cancel" __text="Cancel"}
+        {formbutton commandName="create" __text="Create"}
+        {formbutton commandName="cancel" __text="Cancel"}
     </div>
 </div>
-{/pnform}
+{/form}
