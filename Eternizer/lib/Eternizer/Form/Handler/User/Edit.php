5b89fb73f8b483f38555ad0c1bd5cbe628df0310
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-01-06 18:08:00 +0100

    block of editing of foreign entries

--- Eternizer/lib/Eternizer/Form/Handler/User/Edit.php
+++ Eternizer/lib/Eternizer/Form/Handler/User/Edit.php
@@ -17,5 +17,83 @@
  */
 class Eternizer_Form_Handler_User_Edit extends Eternizer_Form_Handler_User_Base_Edit
 {
-    // feel free to extend the base handler class here
+    /**
+     * Initialize form handler.
+     *
+     * This method takes care of all necessary initialisation of our data and form states.
+     *
+     * @return boolean False in case of initialization errors, otherwise true.
+     */
+    public function initialize(Zikula_Form_View $view)
+    {
+        $this->inlineUsage = ((UserUtil::getTheme() == 'Printer') ? true : false);
+        $this->idPrefix = $this->request->getGet()->filter('idp', '', FILTER_SANITIZE_STRING);
+
+        // initialise redirect goal
+        $this->returnTo = $this->request->getGet()->filter('returnTo', null, FILTER_SANITIZE_STRING);
+        // store current uri for repeated creations
+        $this->repeatReturnUrl = System::getCurrentURI();
+
+        $this->permissionComponent = $this->name . ':' . $this->objectTypeCapital . ':';
+
+        $entityClass = $this->name . '_Entity_' . ucfirst($this->objectType);
+        $objectTemp = new $entityClass();
+        $this->idFields = $objectTemp->get_idFields();
+
+        // retrieve identifier of the object we wish to view
+        $this->idValues = Eternizer_Util_Controller::retrieveIdentifier($this->request, array(), $this->objectType, $this->idFields);
+        $hasIdentifier = Eternizer_Util_Controller::isValidIdentifier($this->idValues);
+
+        $entity = null;
+        $this->mode = ($hasIdentifier) ? 'edit' : 'create';
+
+        if ($this->mode == 'edit') {
+            if (!SecurityUtil::checkPermission($this->permissionComponent, '::', ACCESS_EDIT)) {
+                // set an error message and return false
+                return LogUtil::registerPermissionError();
+            }
+
+            $entity = $this->initEntityForEdit();
+
+            if ($this->hasPageLockSupport === true && ModUtil::available('PageLock')) {
+                // try to guarantee that only one person at a time can be editing this entity
+                ModUtil::apiFunc('PageLock', 'user', 'pageLock',
+                                         array('lockName' => $this->name . $this->objectTypeCapital . $this->createCompositeIdentifier(),
+                                               'returnUrl' => $this->getRedirectUrl(null, $entity)));
+            }
+        }
+        else {
+            if (!SecurityUtil::checkPermission($this->permissionComponent, '::', ACCESS_ADD)) {
+                return LogUtil::registerPermissionError();
+            }
+
+            $entity = $this->initEntityForCreation($entityClass);
+        }
+
+        $this->view->assign('mode', $this->mode)
+                   ->assign('inlineUsage', $this->inlineUsage);
+
+        $entityData = $entity->toArray();
+        
+        // Own code to handle editing of entries only for the creater
+        
+        $userid = UserUtil::getVar('uid');
+        
+        if ($entityData['createdUserId'] != $userid && $this->mode != 'create') {
+        	
+        	$url = ModUtil::url($this->name, 'user', 'view');
+        	return System::redirect($url);
+        }
+
+        // assign data to template as array (makes translatable support easier)
+        $this->view->assign($this->objectTypeLower, $entityData);
+
+        // save entity reference for later reuse
+        $this->entityRef = $entity;
+
+        $this->initializeAdditions();
+
+        // everything okay, no initialization errors occured
+        return true;
+    }
 }
