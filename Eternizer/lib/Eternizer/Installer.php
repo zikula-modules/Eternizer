8205edd0d85d0165fa359775ed32f5c504e6faec
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-01-13 09:15:23 +0100

    old upgrade cases

--- Eternizer/lib/Eternizer/Installer.php
+++ Eternizer/lib/Eternizer/Installer.php
@@ -16,5 +16,45 @@
  */
 class Eternizer_Installer extends Eternizer_Base_Installer
 {
-    // feel free to extend the installer here
+    /**
+     * Upgrade the Eternizer application from an older version.
+     *
+     * If the upgrade fails at some point, it returns the last upgraded version.
+     *
+     * @param integer $oldversion Version to upgrade from.
+     *
+     * @return boolean True on success, false otherwise.
+     */
+    public function upgrade($oldversion)
+    {
+        // Upgrade dependent on old version number
+        switch ($oldversion) {
+        	case '1.0a':
+        	case '1.0':
+        	case '1.1':
+            $profile = ModUtil::getVar('Eternizer', 'profile');
+
+            if (DataUtil::is_serialized($profile)) {
+                $profile = unserialize($profile);
+            }
+            
+            $profile = ModUtil::setVar('Eternizer', 'profile', $profile);
+            case '1.1.1':
+                // do something
+                // ...
+                // update the database schema
+                try {
+                    DoctrineHelper::updateSchema($this->entityManager, $this->listEntityClasses());
+                } catch (Exception $e) {
+                    if (System::isDevelopmentMode()) {
+                        LogUtil::registerError($this->__('Doctrine Exception: ') . $e->getMessage());
+                    }
+                    return LogUtil::registerError($this->__f('An error was encountered while dropping the tables for the %s module.', array($this->getName())));
+                }
+            
+            break;
+        }
+        
+    return true;
+    }
 }
