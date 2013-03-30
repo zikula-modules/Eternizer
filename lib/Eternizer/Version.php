edb39ac42363d5542683a03ecf02730efdf558ff
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-03-30 19:49:41 +0100

    new version

--- lib/Eternizer/Version.php
+++ lib/Eternizer/Version.php
@@ -16,5 +16,42 @@
  */
 class Eternizer_Version extends Eternizer_Base_Version
 {
-    // custom enhancements can go here
+    public function getMetaData()
+    {
+        $meta = array();
+        // the current module version
+        $meta['version'] = '1.1.3';
+        // the displayed name of the module
+        $meta['displayname'] = $this->__('Eternizer');
+        // the module description
+        $meta['description'] = $this->__('Eternizer - A guestbook module');
+        //! url version of name, should be in lowercase without space
+        $meta['url'] = $this->__('eternizer');
+        // core requirement
+        $meta['core_min'] = '1.3.1'; // requires minimum 1.3.1 or later
+        $meta['core_max'] = '1.3.99'; // not ready for 1.4.0 yet
+
+        // define special capabilities of this module
+        $meta['capabilities'] = array(
+                HookUtil::SUBSCRIBER_CAPABLE => array('enabled' => true)
+                /*,
+                 HookUtil::PROVIDER_CAPABLE => array('enabled' => true), // TODO: see #15
+        'authentication' => array('version' => '1.0'),
+        'profile'        => array('version' => '1.0', 'anotherkey' => 'anothervalue'),
+        'message'        => array('version' => '1.0', 'anotherkey' => 'anothervalue')
+        */
+        );
+
+        // permission schema
+        // DEBUG: permission schema aspect starts
+        $meta['securityschema'] = array(
+                'Eternizer::' => '::',
+
+                'Eternizer:Entry:' => 'EntryID::'
+        );
+        // DEBUG: permission schema aspect ends
+
+
+        return $meta;
+    }
 }
