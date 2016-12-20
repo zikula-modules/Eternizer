820c8e42e30e3d29ee49c5107e2fe7ca69e49874
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2016-12-20 21:07:35 +0100

    resources updated

--- Resources/public/js/MUEternizerModule.Validation.js
+++ Resources/public/js/MUEternizerModule.Validation.js
@@ -50,7 +50,7 @@ function mUEternizerReadDate(val, includeTime)
     // look if we have DD.MM.YYYY
     if (val.substr(2, 1) === '.' && val.substr(5, 1) === '.') {
         var newVal = val.substr(6, 4) + '-' + val.substr(3, 2) + '-' + val.substr(0, 2);
-        if (includeTime === true) {
+        if (true === includeTime) {
             newVal += ' ' + val.substr(11, 5);
         }
 
