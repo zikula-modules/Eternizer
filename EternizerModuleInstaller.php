4a9c1466c5888d54ab4762cd1fbc83d39c691982
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2017-01-01 22:47:53 +0100

    old stuff and standard entries per page in update

--- EternizerModuleInstaller.php
+++ EternizerModuleInstaller.php
@@ -16,6 +16,7 @@ use MU\EternizerModule\Base\AbstractEternizerModuleInstaller;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use DBUtil;
 
 /**
  * Installer implementation class.
@@ -54,6 +55,52 @@ class EternizerModuleInstaller extends AbstractEternizerModuleInstaller
     
         // Upgrade dependent on old version number
         switch ($oldVersion) {
+        	case '1.0a':
+        	case '1.0':
+        	case '1.1':
+        		$profile = ModUtil::getVar('Eternizer', 'profile');
+        	
+        		if (DataUtil::is_serialized($profile)) {
+        			$profile = unserialize($profile);
+        		}
+        	
+        		$profile = ModUtil::setVar('Eternizer', 'profile', $profile);
+        		
+        	case '1.1.1':
+        	
+        		// remove all module vars
+        		$this->delVars();
+        	
+        		// create all tables from according entity definitions
+        		try {
+        			DoctrineHelper::createSchema($this->entityManager, $this->listEntityClasses());
+        		} catch (Exception $e) {
+        			if (System::isDevelopmentMode()) {
+        				LogUtil::registerError($this->__('Doctrine Exception: ') . $e->getMessage());
+        			}
+        			return LogUtil::registerError($this->__f('An error was encountered while creating the tables for the %s module.', array($this->getName())));
+        		}
+        	
+        		// set up all our vars with initial values
+        		Eternizer_Util_Controller::setModVars();
+        	
+        		// create the default data for Eternizer
+        		$this->createDefaultData();
+        	
+        		// register persistent event handlers
+        		$this->registerPersistentEventHandlers();
+        	
+        		// register hook subscriber bundles
+        		HookUtil::registerSubscriberBundles($this->version->getHookSubscriberBundles());
+        	
+        	case '1.1.2':
+        		ModUtil::setVar('Eternizer', 'period', 12);
+        		ModUtil::setVar('Eternizer', 'simplecaptcha', false);
+        		$this->createTempDir();
+        	
+        	case '1.1.3':
+        		// nothing to do
+        		
             case '1.1.4':
                 
             	// rename module for all modvars
@@ -61,8 +108,12 @@ class EternizerModuleInstaller extends AbstractEternizerModuleInstaller
             	
             	// handle renamed and deleted modvars
             	$pageSize = $this->getVar('pagesize');
+            	if ($pageSize != '') {
+            	    $this->setVar('entryEntriesPerPage', $pageSize);
+            	} else {
+            		$this->setVar('entryEntriesPerPage', 10);
+            	}
             	$this->delVar('pagesize');
-            	$this->setVar('entryEntriesPerPage', $pageSize);
             	
             	$this->setVar('moderationGroupForEntries', 2);
             	$this->addFlash('status', __('The group of adminstrators will get emails now, if someone submit an entry. The setting variable for an email was deleted! You are able to change the group of moderators in the settings of this module'));           	
@@ -99,11 +150,28 @@ class EternizerModuleInstaller extends AbstractEternizerModuleInstaller
                     return false;
                 }
                 
+                $dbName = $this->getDbName();
+                $conn = $this->getConnection();
+                	
+                $conn->executeQuery("
+                		UPDATE $dbName.mu_eternizer_entry
+                		SET workflowState = 'approved';
+                		");
+                                
+                /*$result2 = DBUtil::executeSQL('SELECT * FROM `mu_eternizer_entry`');
+                $entries = $result2->fetchAll(Doctrine::FETCH_ASSOC);
+                
+		        // we set the workflow
+                foreach ($entries as $key => $entry) {
+                	$sql[] = "INSERT INTO workflows (spalte1, spalte2, splate3) VALUES ($tr, ?, ?)";
+
+                }TODO Workflow table entries*/
+                
             case '1.4.0':
             	// for later updates
         }
     
-
+        // update successful
         return true;
     }
     
