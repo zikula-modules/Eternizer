4871bcd964658d066fb0db9583f13f16d0c9feab
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2017-01-11 17:02:31 +0100

    no standard sortdir in route

--- Controller/EntryController.php
+++ Controller/EntryController.php
@@ -18,17 +18,14 @@ use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
-use ModUtil;
-use System;
+use Zikula\ThemeModule\Engine\Annotation\Theme;
+use Zikula\Core\RouteUrl;
+use MU\EternizerModule\Entity\EntryEntity;
 use Zikula\Component\SortableColumns\Column;
 use Zikula\Component\SortableColumns\SortableColumns;
-use Zikula\Core\RouteUrl;
-use Zikula\ThemeModule\Engine\Annotation\Theme;
-
-use MU\EternizerModule\Entity\EntryEntity;
+use System;
+use ModUtil;
 
 /**
  * Entry controller class providing navigation and interaction functionality.
@@ -101,7 +98,7 @@ class EntryController extends AbstractEntryController
      *
      * @Route("/entries/view/{sort}/{sortdir}/{pos}/{num}.{_format}",
      *        requirements = {"sortdir" = "asc|desc|ASC|DESC", "pos" = "\d+", "num" = "\d+", "_format" = "html|csv|rss|atom|xml|json|kml"},
-     *        defaults = {"sort" = "", "sortdir" = "asc", "pos" = 1, "num" = 10, "_format" = "html"},
+     *        defaults = {"sort" = "", "sortdir" = "", "pos" = 1, "num" = 10, "_format" = "html"},
      *        methods = {"GET"}
      * )
      *
@@ -119,6 +116,189 @@ class EntryController extends AbstractEntryController
     {
         return parent::viewAction($request, $sort, $sortdir, $pos, $num);
     }
+    
+    /**
+     * This method includes the common implementation code for adminView() and view().
+     */
+    protected function viewInternal(Request $request, $sort, $sortdir, $pos, $num, $isAdmin = false)
+    {
+    	// parameter specifying which type of objects we are treating
+    	$objectType = 'entry';
+    	$utilArgs = ['controller' => 'entry', 'action' => 'view'];
+    	$permLevel = $isAdmin ? ACCESS_ADMIN : ACCESS_READ;
+    	if (!$this->hasPermission($this->name . ':' . ucfirst($objectType) . ':', '::', $permLevel)) {
+    		throw new AccessDeniedException();
+    	}
+    	$repository = $this->get('mu_eternizer_module.' . $objectType . '_factory')->getRepository();
+    	$repository->setRequest($request);
+    	$viewHelper = $this->get('mu_eternizer_module.view_helper');
+    	$templateParameters = [
+    			'routeArea' => $isAdmin ? 'admin' : ''
+    	];
+    	$selectionHelper = $this->get('mu_eternizer_module.selection_helper');
+    
+    	// convenience vars to make code clearer
+    	$currentUrlArgs = [];
+    	$where = '';
+    
+    	$showOwnEntries = $request->query->getInt('own', $this->getVar('showOnlyOwnEntries', 0));
+    	$showAllEntries = $request->query->getInt('all', 0);
+    
+    	if (!$showAllEntries) {
+    		$csv = $request->getRequestFormat() == 'csv' ? 1 : 0;
+    		if ($csv == 1) {
+    			$showAllEntries = 1;
+    		}
+    	}
+    
+    	$templateParameters['own'] = $showAllEntries;
+    	$templateParameters['all'] = $showOwnEntries;
+    	if ($showAllEntries == 1) {
+    		$currentUrlArgs['all'] = 1;
+    	}
+    	if ($showOwnEntries == 1) {
+    		$currentUrlArgs['own'] = 1;
+    	}
+    
+    	$additionalParameters = $repository->getAdditionalTemplateParameters('controllerAction', $utilArgs);
+    
+    	$resultsPerPage = 0;
+    	if ($showAllEntries != 1) {
+    		// the number of items displayed on a page for pagination
+    		$resultsPerPage = $num;
+    		if (in_array($resultsPerPage, [0, 10])) {
+    			$resultsPerPage = $this->getVar($objectType . 'EntriesPerPage', 10);
+    		}
+    	}
+    
+    	// parameter for used sorting field
+    	if (empty($sort) || !in_array($sort, $repository->getAllowedSortingFields())) {
+    		$sort = $repository->getDefaultSortingField();
+    		System::queryStringSetVar('sort', $sort);
+    		$request->query->set('sort', $sort);
+    		// set default sorting in route parameters (e.g. for the pager)
+    		$routeParams = $request->attributes->get('_route_params');
+    		$routeParams['sort'] = $sort;
+    		$request->attributes->set('_route_params', $routeParams);
+    	}
+    
+    	// parameter for used sort order
+    	    $modSortDir = \ModUtil::getVar($this->name, 'order');
+    	    if ($modSortDir != '') {
+    		    if ($modSortDir == 'descending') {
+    			    $sortdir = 'desc';
+    		    } else {
+    			    $sortdir = 'asc';
+    		    }
+    	    } else {
+    	    	$sortdir = 'desc';
+    	    }
+    
+    	$sortableColumns = new SortableColumns($this->get('router'), 'mueternizermodule_entry_' . ($isAdmin ? 'admin' : '') . 'view', 'sort', 'sortdir');
+    	$sortableColumns->addColumns([
+    			new Column('workflowState'),
+    			new Column('ip'),
+    			new Column('name'),
+    			new Column('email'),
+    			new Column('homepage'),
+    			new Column('location'),
+    			new Column('text'),
+    			new Column('notes'),
+    			new Column('createdBy'),
+    			new Column('createdDate'),
+    			new Column('updatedBy'),
+    			new Column('updatedDate'),
+    	]);
+    
+    	$additionalUrlParameters = [
+    			'all' => $showAllEntries,
+    			'own' => $showOwnEntries,
+    			'num' => $resultsPerPage
+    	];
+    	foreach ($additionalParameters as $parameterName => $parameterValue) {
+    		if (false !== stripos($parameterName, 'thumbRuntimeOptions')) {
+    			continue;
+    		}
+    		$additionalUrlParameters[$parameterName] = $parameterValue;
+    	}
+    
+    	$templateParameters['sort'] = $sort;
+    	$templateParameters['sortdir'] = $sortdir;
+    	$templateParameters['num'] = $resultsPerPage;
+    
+    	$tpl = '';
+    	if ($request->isMethod('POST')) {
+    		$tpl = $request->request->getAlnum('tpl', '');
+    	} elseif ($request->isMethod('GET')) {
+    		$tpl = $request->query->getAlnum('tpl', '');
+    	}
+    	$templateParameters['tpl'] = $tpl;
+    
+    	$quickNavForm = $this->createForm('MU\EternizerModule\Form\Type\QuickNavigation\\' . ucfirst($objectType) . 'QuickNavType', $templateParameters);
+    	if ($quickNavForm->handleRequest($request) && $quickNavForm->isSubmitted()) {
+    		$quickNavData = $quickNavForm->getData();
+    		foreach ($quickNavData as $fieldName => $fieldValue) {
+    			if ($fieldName == 'routeArea') {
+    				continue;
+    			}
+    			if ($fieldName == 'all') {
+    				$showAllEntries = $additionalUrlParameters['all'] = $templateParameters['all'] = $fieldValue;
+    			} elseif ($fieldName == 'own') {
+    				$showOwnEntries = $additionalUrlParameters['own'] = $templateParameters['own'] = $fieldValue;
+    			} elseif ($fieldName == 'num') {
+    				$resultsPerPage = $additionalUrlParameters['num'] = $fieldValue;
+    			} else {
+    				// set filter as query argument, fetched inside repository
+    				$request->query->set($fieldName, $fieldValue);
+    			}
+    		}
+    	}
+    	$sortableColumns->setOrderBy($sortableColumns->getColumn($sort), strtoupper($sortdir));
+    	$sortableColumns->setAdditionalUrlParameters($additionalUrlParameters);
+    
+    	if ($showAllEntries == 1) {
+    		// retrieve item list without pagination
+    		$entities = $selectionHelper->getEntities($objectType, [], $where, $sort . ' ' . $sortdir);
+    	} else {
+    		// the current offset which is used to calculate the pagination
+    		$currentPage = $pos;
+    
+    		// retrieve item list with pagination
+    		list($entities, $objectCount) = $selectionHelper->getEntitiesPaginated($objectType, $where, $sort . ' ' . $sortdir, $currentPage, $resultsPerPage);
+    
+    		$templateParameters['currentPage'] = $currentPage;
+    		$templateParameters['pager'] = ['numitems' => $objectCount, 'itemsperpage' => $resultsPerPage];
+    	}
+    
+    	foreach ($entities as $k => $entity) {
+    		$entity->initWorkflow();
+    	}
+    
+    	// build RouteUrl instance for display hooks
+    	$currentUrlArgs['_locale'] = $request->getLocale();
+    	$currentUrlObject = new RouteUrl('mueternizermodule_entry_' . /*($isAdmin ? 'admin' : '') . */'view', $currentUrlArgs);
+    
+    	$templateParameters['items'] = $entities;
+    	$templateParameters['sort'] = $sort;
+    	$templateParameters['sortdir'] = $sortdir;
+    	$templateParameters['num'] = $resultsPerPage;
+    	$templateParameters['currentUrlObject'] = $currentUrlObject;
+    	$templateParameters = array_merge($templateParameters, $additionalParameters);
+    
+    	$templateParameters['sort'] = $sortableColumns->generateSortableColumns();
+    	$templateParameters['quickNavForm'] = $quickNavForm->createView();
+    
+    	$templateParameters['showAllEntries'] = $templateParameters['all'];
+    	$templateParameters['showOwnEntries'] = $templateParameters['own'];
+    
+    	$modelHelper = $this->get('mu_eternizer_module.model_helper');
+    	$templateParameters['canBeCreated'] = $modelHelper->canBeCreated($objectType);
+    
+    	// fetch and return the appropriate template
+    	return $viewHelper->processTemplate($this->get('twig'), $objectType, 'view', $request, $templateParameters);
+    }
+    
+    
     /**
      * This action provides a item detail view in the admin area.
      *
@@ -295,175 +475,5 @@ class EntryController extends AbstractEntryController
         return parent::handleSelectedEntriesAction($request);
     }
 
-   /**
-     * This method includes the common implementation code for adminView() and view().
-     */
-    protected function viewInternal(Request $request, $sort, $sortdir, $pos, $num, $isAdmin = false)
-    {
-        // parameter specifying which type of objects we are treating
-        $objectType = 'entry';
-        $utilArgs = ['controller' => 'entry', 'action' => 'view'];
-        $permLevel = $isAdmin ? ACCESS_ADMIN : ACCESS_READ;
-        if (!$this->hasPermission($this->name . ':' . ucfirst($objectType) . ':', '::', $permLevel)) {
-            throw new AccessDeniedException();
-        }
-        $repository = $this->get('mu_eternizer_module.' . $objectType . '_factory')->getRepository();
-        $repository->setRequest($request);
-        $viewHelper = $this->get('mu_eternizer_module.view_helper');
-        $templateParameters = [
-            'routeArea' => $isAdmin ? 'admin' : ''
-        ];
-        $selectionHelper = $this->get('mu_eternizer_module.selection_helper');
-        
-        // convenience vars to make code clearer
-        $currentUrlArgs = [];
-        $where = '';
-        
-        $showOwnEntries = $request->query->getInt('own', $this->getVar('showOnlyOwnEntries', 0));
-        $showAllEntries = $request->query->getInt('all', 0);
-        
-        if (!$showAllEntries) {
-            $csv = $request->getRequestFormat() == 'csv' ? 1 : 0;
-            if ($csv == 1) {
-                $showAllEntries = 1;
-            }
-        }
-        
-        $templateParameters['own'] = $showAllEntries;
-        $templateParameters['all'] = $showOwnEntries;
-        if ($showAllEntries == 1) {
-            $currentUrlArgs['all'] = 1;
-        }
-        if ($showOwnEntries == 1) {
-            $currentUrlArgs['own'] = 1;
-        }
-        
-        $additionalParameters = $repository->getAdditionalTemplateParameters('controllerAction', $utilArgs);
-        
-        $resultsPerPage = 0;
-        if ($showAllEntries != 1) {
-            // the number of items displayed on a page for pagination
-            $resultsPerPage = $num;
-            if (in_array($resultsPerPage, [0, 10])) {
-                $resultsPerPage = $this->getVar($objectType . 'EntriesPerPage', 10);
-            }
-        }
-        
-        // parameter for used sorting field
-        if (empty($sort) || !in_array($sort, $repository->getAllowedSortingFields())) {
-            $sort = $repository->getDefaultSortingField();
-            System::queryStringSetVar('sort', $sort);
-            $request->query->set('sort', $sort);
-            // set default sorting in route parameters (e.g. for the pager)
-            $routeParams = $request->attributes->get('_route_params');
-            $routeParams['sort'] = $sort;
-            $request->attributes->set('_route_params', $routeParams);
-        }
-        
-        // parameter for used sort order
-        $sortdir = strtolower($sortdir);
-        
-        $sortableColumns = new SortableColumns($this->get('router'), 'mueternizermodule_entry_' . ($isAdmin ? 'admin' : '') . 'view', 'sort', 'sortdir');
-        $sortableColumns->addColumns([
-            new Column('workflowState'),
-            new Column('ip'),
-            new Column('name'),
-            new Column('email'),
-            new Column('homepage'),
-            new Column('location'),
-            new Column('text'),
-            new Column('notes'),
-            new Column('createdBy'),
-            new Column('createdDate'),
-            new Column('updatedBy'),
-            new Column('updatedDate'),
-        ]);
-        
-        $additionalUrlParameters = [
-            'all' => $showAllEntries,
-            'own' => $showOwnEntries,
-            'num' => $resultsPerPage
-        ];
-        foreach ($additionalParameters as $parameterName => $parameterValue) {
-            if (false !== stripos($parameterName, 'thumbRuntimeOptions')) {
-                continue;
-            }
-            $additionalUrlParameters[$parameterName] = $parameterValue;
-        }
-        
-        $templateParameters['sort'] = $sort;
-        $templateParameters['sortdir'] = $sortdir;
-        $templateParameters['num'] = $resultsPerPage;
-        
-        $tpl = '';
-        if ($request->isMethod('POST')) {
-            $tpl = $request->request->getAlnum('tpl', '');
-        } elseif ($request->isMethod('GET')) {
-            $tpl = $request->query->getAlnum('tpl', '');
-        }
-        $templateParameters['tpl'] = $tpl;
-        
-        $quickNavForm = $this->createForm('MU\EternizerModule\Form\Type\QuickNavigation\\' . ucfirst($objectType) . 'QuickNavType', $templateParameters);
-        if ($quickNavForm->handleRequest($request) && $quickNavForm->isSubmitted()) {
-            $quickNavData = $quickNavForm->getData();
-            foreach ($quickNavData as $fieldName => $fieldValue) {
-                if ($fieldName == 'routeArea') {
-                    continue;
-                }
-                if ($fieldName == 'all') {
-                    $showAllEntries = $additionalUrlParameters['all'] = $templateParameters['all'] = $fieldValue;
-                } elseif ($fieldName == 'own') {
-                    $showOwnEntries = $additionalUrlParameters['own'] = $templateParameters['own'] = $fieldValue;
-                } elseif ($fieldName == 'num') {
-                    $resultsPerPage = $additionalUrlParameters['num'] = $fieldValue;
-                } else {
-                    // set filter as query argument, fetched inside repository
-                    $request->query->set($fieldName, $fieldValue);
-                }
-            }
-        }
-        $sortableColumns->setOrderBy($sortableColumns->getColumn($sort), strtoupper($sortdir));
-        $sortableColumns->setAdditionalUrlParameters($additionalUrlParameters);
-        
-        if ($showAllEntries == 1) {
-            // retrieve item list without pagination
-            $entities = $selectionHelper->getEntities($objectType, [], $where, $sort . ' ' . $sortdir);
-        } else {
-            // the current offset which is used to calculate the pagination
-            $currentPage = $pos;
-        
-            // retrieve item list with pagination
-            list($entities, $objectCount) = $selectionHelper->getEntitiesPaginated($objectType, $where, $sort . ' ' . $sortdir, $currentPage, $resultsPerPage);
-        
-            $templateParameters['currentPage'] = $currentPage;
-            $templateParameters['pager'] = ['numitems' => $objectCount, 'itemsperpage' => $resultsPerPage];
-        }
-        
-        foreach ($entities as $k => $entity) {
-            $entity->initWorkflow();
-        }
-        
-        // build RouteUrl instance for display hooks
-        $currentUrlArgs['_locale'] = $request->getLocale();
-        $currentUrlObject = new RouteUrl('mueternizermodule_entry_' . /*($isAdmin ? 'admin' : '') . */'view', $currentUrlArgs);
-        
-        $templateParameters['items'] = $entities;
-        $templateParameters['sort'] = $sort;
-        $templateParameters['sortdir'] = $sortdir;
-        $templateParameters['num'] = $resultsPerPage;
-        $templateParameters['currentUrlObject'] = $currentUrlObject;
-        $templateParameters = array_merge($templateParameters, $additionalParameters);
-        
-        $templateParameters['sort'] = $sortableColumns->generateSortableColumns();
-        $templateParameters['quickNavForm'] = $quickNavForm->createView();
-        
-        $templateParameters['showAllEntries'] = $templateParameters['all'];
-        $templateParameters['showOwnEntries'] = $templateParameters['own'];
-        
-        $modelHelper = $this->get('mu_eternizer_module.model_helper');
-        $templateParameters['canBeCreated'] = $modelHelper->canBeCreated($objectType);
-        
-        // fetch and return the appropriate template
-        return $viewHelper->processTemplate($this->get('twig'), $objectType, 'view', $request, $templateParameters);
-    }
+    // feel free to add your own controller methods here
 }
